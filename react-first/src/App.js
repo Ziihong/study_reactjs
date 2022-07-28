0c70e85476520ced3f43eb09afd4b6ccfe136872
jihong <rlawyd9@kookmin.ac.kr>
2022-07-28 23:30:35 +0900

    useEffect

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,11 +1,43 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+
+import {useEffect, useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const handleClick = () => {
+        setCounter((current) => current+1)
+    };
+    const handleChange = (event) => {
+        setKeyword(event.target.value);
+    }
+
+    // useEffect를 이용하여 해당 코드를 딱 한 번만 실행할 수 있다.
+    useEffect(() => {
+        console.log("CALL THE API AREA . . . // I run only once")
+    }, [])
+
+    // useEffect의 dependencies 인자를 이용하여 특정 state가 변화할 때만 해당 코드를 실행하도록 설정할 수 있다.
+    useEffect(() => {
+        console.log("SEARCH FOR . . . // I run when 'keyword' changes");
+    }, [keyword])
+    useEffect(() => {
+        console.log("Click button . . . // I run when 'counter' changes");
+    }, [counter])
+    useEffect(() => {
+        console.log("I run when 'keyword'&'counter' changes");
+    },[keyword, counter])
+
+
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
-            <Button text={"Continue"}></Button>
+            <input
+                value={keyword}
+                onChange={handleChange}
+                type="text"
+                placeholder="Search here🔎"
+            />
+            <h1>Count: {counter}</h1>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
--- react-first/src/Button.js
+++ react-first/src/Button.js
@@ -1,17 +1,17 @@
-import PropTypes from "prop-types";
-import styles from "./Button.module.css"
-// create-react-app은 css코드를 javascript 오브젝트로 변환시켜준다. -> 'styles'
-// 'styles' 오브젝트는 안에 btn을 갖고 있다.
-// 그리고 create-react-app은 랜덤하게 class 이름을 만들어낸다.
-
-function Button({text}){
-    return (
-        <button className={styles.btn}>{text}</button>
-    )
-}
-
-Button.propTypes = {
-    text: PropTypes.string.isRequired,
-};
-
-export default Button;
+// import PropTypes from "prop-types";
+// import styles from "./Button.module.css"
+// // create-react-app은 css코드를 javascript 오브젝트로 변환시켜준다. -> 'styles'
+// // 'styles' 오브젝트는 안에 btn을 갖고 있다.
+// // 그리고 create-react-app은 랜덤하게 class 이름을 만들어낸다.
+//
+// function Button({text}){
+//     return (
+//         <button className={styles.btn}>{text}</button>
+//     )
+// }
+//
+// Button.propTypes = {
+//     text: PropTypes.string.isRequired,
+// };
+//
+// export default Button;

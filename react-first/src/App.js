69fad1de3fc21d533a1eb922d7979aa393d3ea52
jihong <rlawyd9@kookmin.ac.kr>
2022-08-08 23:53:54 +0900

    PRACTICE_coinTracker

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,33 +1,28 @@
 import {useState} from "react";
+import {useEffect} from "react";
 
 function App(){
-    // 절대 state를 직접적으로 수정하지 않는다. set 함수를 이용해야한다. ex) toDo = "" (x)
-    const [toDo, setToDo] = useState("");
-    const [toDoList, setToDoList] = useState([]); // to do list는 array로 저장
-    const handleChange = (event) => setToDo(event.target.value);
-    const handleSubmit = (event) => {
-        event.preventDefault(); // 자동 새로고침 방지
-        if(toDo === "") return;
-        // ...을 사용하여 array 안의 내용만 가져올 수 있다.
-        // ex) currentArray = [1, 2, 3]  ->  ...currentArray = 1, 2, 3
-        setToDoList((currentArray) => [toDo, ...currentArray]);
-        setToDo("");
-    };
-
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([])
+    useEffect(() => {
+        // api 한 번만 호출하기
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, [])
     return(
         <div>
-            <h1>My To Do List({toDoList.length})</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    onChange={handleChange}
-                    value={toDo} type="text"
-                    placeholder="Write your to do ..."
-                />
-                <button>Add To Do</button>
-            </form>
-            <hr/>
+            <h1>The Coins! ({coins.length})</h1>
+            {loading ? <strong>Loading . . .</strong> : null}
             <ul>
-                {toDoList.map((item, index) => <li key={index}>{item}</li>)}
+                {coins.map((coin) => (
+                    <li>
+                        {coin.name} ({coin.symbol}: ${coin.quotes.USD.price} USD)
+                    </li>
+                ))}
             </ul>
         </div>
     )

4cce320f13885173902ea455ffb7ab47f91d01ce
jihong <rlawyd9@kookmin.ac.kr>
2022-08-08 23:27:18 +0900

    PRACTICE_toDoList

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,41 +1,36 @@
+import {useState} from "react";
 
-import {useEffect, useState} from "react";
+function App(){
+    // 절대 state를 직접적으로 수정하지 않는다. set 함수를 이용해야한다. ex) toDo = "" (x)
+    const [toDo, setToDo] = useState("");
+    const [toDoList, setToDoList] = useState([]); // to do list는 array로 저장
+    const handleChange = (event) => setToDo(event.target.value);
+    const handleSubmit = (event) => {
+        event.preventDefault(); // 자동 새로고침 방지
+        if(toDo === "") return;
+        // ...을 사용하여 array 안의 내용만 가져올 수 있다.
+        // ex) currentArray = [1, 2, 3]  ->  ...currentArray = 1, 2, 3
+        setToDoList((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
 
-function Greeting(){
-    // clean up function: 해당 컴포넌트가 사라질때 뭔가 실행되도록 할 수 있다.
-
-    // 첫번째 방법
-    useEffect(() => {
-        console.log("created");
-        return () => console.log("destroyed");
-    }, [])
-
-    // 두번째 방법
-    function destroyFn(){
-        console.log("destroyed")
-    }
-    function createFn(){
-        console.log("created");
-        return destroyFn;
-    } // 컴포넌트가 사라질 때 실행될 함수를 return 해주어야 한다!
-    useEffect(createFn, [])
-
-    return (
-        <h1>Hello</h1>
-    );
-}
-
-
-function App() {
-    const [showing, setShowing] = useState(false);
-    const handleClick = () => setShowing((prev) => !prev)
-
-    return (
+    return(
         <div>
-            {showing ? <Greeting/> : null}
-            <button onClick={handleClick}>{showing ? "Hide" : "Show"}</button>
+            <h1>My To Do List({toDoList.length})</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={handleChange}
+                    value={toDo} type="text"
+                    placeholder="Write your to do ..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr/>
+            <ul>
+                {toDoList.map((item, index) => <li key={index}>{item}</li>)}
+            </ul>
         </div>
-    );
+    )
 }
 
 export default App;

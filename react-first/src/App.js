01886b5b085fba697bdaf662f967b936b705551d
jihong <rlawyd9@kookmin.ac.kr>
2022-08-10 23:35:12 +0900

    PRACTICE_coinTracker_select

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {useEffect} from "react";
 
+
 function App(){
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([])
@@ -15,15 +16,17 @@ function App(){
     }, [])
     return(
         <div>
-            <h1>The Coins! ({coins.length})</h1>
-            {loading ? <strong>Loading . . .</strong> : null}
-            <ul>
-                {coins.map((coin) => (
-                    <li>
-                        {coin.name} ({coin.symbol}: ${coin.quotes.USD.price} USD)
-                    </li>
-                ))}
-            </ul>
+            <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+            {loading ? <strong>Loading . . .</strong>
+                :
+                <select>
+                    {coins.map((coin) => (
+                        <option>
+                            {coin.name} ({coin.symbol}: ${coin.quotes.USD.price} USD)
+                        </option>
+                    ))}
+                </select>
+            }
         </div>
     )
 }

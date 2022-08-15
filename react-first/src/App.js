77c603965fe4cc88a55a2fb397a40481da699d6d
jihong <rlawyd9@kookmin.ac.kr>
2022-08-15 23:42:02 +0900

    PRACTICE_movie app_router2

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,10 +1,29 @@
-import {useState} from "react";
-import {useEffect} from "react";
-import Movie from './components/Movie';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link // -> Link 컴포넌트는 브라우저 새로고침 없어도 유저를 다른 페이지로 이동시켜주는 컴포넌트이다.
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+// react-router-dom 버전6 이후 변경 사항
+// <Switch></Switch> => <Routes></Routes> 로 변경
+// 렌더링할 컴포넌트 -> Route 컴포넌트 사이에 위치x, Route 컴포넌트의 element prop으로 설정하도록  ex) <Route path="/" element={<Home/>}></Route>
 
-// 이전에는 App.js에서 영화 리스트를 보여주었지만, 이제는 App.js router를 준비한다.
 function App() {
-    return null;
+    // Switch -> Route(url)를 찾고, 해당 컴포넌트를 렌더링함
+    return (
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- react-first/src/components/Movie.js
+++ react-first/src/components/Movie.js
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({coverImg, title, year, summary, genres}){
     return(
         <div>
             <img src={coverImg} alt={title}/>
-            <h2>{title} ({year})</h2>
+            <h2><Link to="/movie">{title}</Link> ({year})</h2>
             <p>{summary}</p>
             <ul>
                 {genres.map((genre) => <li id={genre}>{genre}</li>)}
